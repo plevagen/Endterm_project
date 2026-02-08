bdd6691a7e24cde748f7525366f91edba1eb9dcf
plevagen <varkginglouh@gmail.com>
2026-02-08 17:26:36 +0500

    eeee

---
+++ src/main/java/com/example/music_library/EndTermProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.music_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndTermProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndTermProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/music_library/EndTermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.music_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndTermProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
